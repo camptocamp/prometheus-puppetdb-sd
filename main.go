7f78efa0b2a00052907d42577b1749c189a79b53
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-01-11 15:01:13 +0100

    While loop

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type Node struct {
@@ -13,23 +14,47 @@ type Node struct {
 	Ipaddress string `json:"value"`
 }
 
+const (
+	query = "facts{name='ipaddress' and nodes{resources{type='Class' and title='Collectd'}}}"
+	sleep = 5 * time.Second
+)
+
 func main() {
-	form := strings.NewReader("{\"query\":\"facts{name='ipaddress' and nodes{resources{type='Class' and title='Collectd'}}}\"}")
-
 	client := &http.Client{}
-	req, _ := http.NewRequest("POST", "http://localhost:8080/pdb/query/v4", form)
-	req.Header.Add("Content-Type", "application/json")
 
-	resp, _ := client.Do(req)
-	body, _ := ioutil.ReadAll(resp.Body)
+	for {
+		form := strings.NewReader(fmt.Sprintf("{\"query\":\"%s\"}", query))
+		req, err := http.NewRequest("POST", "http://localhost:8080/pdb/query/v4", form)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		req.Header.Add("Content-Type", "application/json")
 
-	nodes := make([]Node, 0)
-	err := json.Unmarshal(body, &nodes)
-	if err != nil {
-		fmt.Println(err)
-	}
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
 
-	for _, node := range nodes {
-		fmt.Printf("%s => %s\n", node.Certname, node.Ipaddress)
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		nodes := make([]Node, 0)
+		err = json.Unmarshal(body, &nodes)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		for _, node := range nodes {
+			fmt.Printf("%s => %s\n", node.Certname, node.Ipaddress)
+		}
+
+		fmt.Printf("Sleeping for %v", sleep)
+		time.Sleep(sleep)
 	}
 }
